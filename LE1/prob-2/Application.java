d254f685cb3ea48499d79e384066f8007bc44662
221B144 <221B144@juet.co.in>
2025-01-28 11:58:44 +0530

    updating le1 on git

---
+++ LE1/prob-2/Application.java
@@ -0,0 +1,12 @@
+
+class Application
+{
+	public static void main(String[] args) {
+		Mother m = new Mother();
+        
+
+    
+        Child ch = new Child();
+        ch.show(); // it will show the function of mother class 
+	}
+}
